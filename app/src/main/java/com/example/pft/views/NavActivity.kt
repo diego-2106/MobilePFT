0677adfbba4ad92de1666967d46a9f3ac1c78f93
diego-2106 <ez.mm.gg@gmail.com>
2023-10-26 13:23:42 -0300

    Boton cerrar sesion funcional

--- app/src/main/java/com/example/pft/views/NavActivity.kt
+++ app/src/main/java/com/example/pft/views/NavActivity.kt
@@ -1,9 +1,11 @@
 package com.example.pft.views
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
@@ -11,28 +13,29 @@ import com.example.pft.fragments.FragmentoMediciones
 import com.example.pft.R
 import com.google.android.material.navigation.NavigationView
 
-class NavActivity : AppCompatActivity(){
-    lateinit var toggle : ActionBarDrawerToggle
+class NavActivity : AppCompatActivity() {
+    lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.nav_activity)
 
-        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
-        val navView : NavigationView = findViewById(R.id.nav_view)
+        drawerLayout = findViewById(R.id.drawerLayout)
+        navView = findViewById(R.id.nav_view)
 
-
-        toggle = ActionBarDrawerToggle(this,drawerLayout, R.string.open, R.string.close)
-        drawerLayout.addDrawerListener(toggle) /*le defino al toggle como listener para que cambie de icono dependiendo si abre o no la nav*/
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
-        /*Lo que se hace aca es habilitar la flecha para ir hacia atras en caso de que este abierto el menu*/
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        navView.setNavigationItemSelectedListener {
-            when(it.itemId) {
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
                 R.id.nav_home -> {
                     Toast.makeText(applicationContext, "Clickeaste Inicio", Toast.LENGTH_SHORT).show()
-                    // cargar un fragmento
+                    // Cargar un fragmento o realizar otras acciones relacionadas con "Inicio"
                 }
                 R.id.mediciones -> {
                     val fragment = FragmentoMediciones()
@@ -43,29 +46,48 @@ class NavActivity : AppCompatActivity(){
                 }
                 R.id.manual -> {
                     Toast.makeText(applicationContext, "Clickeaste Manual", Toast.LENGTH_SHORT).show()
-                    // cargar un fragmento
+                    // Cargar un fragmento o realizar otras acciones relacionadas con "Manual"
                 }
                 R.id.settings -> {
                     Toast.makeText(applicationContext, "Clickeaste Ajustes", Toast.LENGTH_SHORT).show()
-                    // cargar un fragmento
+                    // Cargar un fragmento o realizar otras acciones relacionadas con "Ajustes"
                 }
                 R.id.version -> {
                     Toast.makeText(applicationContext, "Clickeaste Version", Toast.LENGTH_SHORT).show()
-                    // cargar un fragmento
+                    // Cargar un fragmento o realizar otras acciones relacionadas con "Versión"
+                }
+                R.id.logout -> {
+                    mostrarMensajeLogout()
                 }
             }
-            drawerLayout.closeDrawer(GravityCompat.START)  // Cierra el drawer
-            true  // selección del ítem
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
         }
-
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(toggle.onOptionsItemSelected(item)) {
+        if (toggle.onOptionsItemSelected(item)) {
             return true
-
         }
         return super.onOptionsItemSelected(item)
     }
 
+    private fun mostrarMensajeLogout() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Cerrar Sesión")
+        builder.setMessage("¿Estás seguro de que deseas cerrar sesión?")
+        builder.setIcon(R.drawable.ic_baseline_warning_24)
+
+        builder.setPositiveButton("Cerrar Sesión") { dialogInterface, i ->
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        builder.setNegativeButton("Cancelar") { dialogInterface, i ->
+            dialogInterface.dismiss()
+        }
+
+        builder.create().show()
+    }
 }
